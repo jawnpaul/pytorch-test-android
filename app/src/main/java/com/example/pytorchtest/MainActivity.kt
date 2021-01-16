6847641f222ffff404bda12ec7706590ffd9a6e8
John Paul <jpaul5246@gmail.com>
2021-01-16 12:09:01 +0100

    Set up UI and navigation for moving to MobileNetActivity

--- app/src/main/java/com/example/pytorchtest/MainActivity.kt
+++ app/src/main/java/com/example/pytorchtest/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.pytorchtest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.pytorchtest.databinding.ActivityMainBinding
+import com.example.pytorchtest.mobilenet.MobileNetActivity
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.mobileNetCardview.setOnClickListener {
+            val intent = Intent(this, MobileNetActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/pytorchtest/mobilenet/MobileNetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pytorchtest.mobilenet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pytorchtest.R
+
+class MobileNetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobile_net)
+    }
+}
